d0059374d0af0da3c623a3bcb6cca9c6af602b31
u25ai003-nisarg <u25ai003@aid.svnit.ac.in>
2026-03-18 13:57:24 +0530

    LAB 1 uploaded

---
+++ OOPs/L1/OOP1.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class OOP1
+{
+    public static void main(String []args)
+    {
+        System.out.println("please enter the number 1");
+        Scanner scn1 = new Scanner(System.in);
+        int n1 = scn1.nextInt();
+        System.out.println("please enter the number 2");
+        Scanner scn2 = new Scanner(System.in);
+        int n2 = scn2.nextInt();
+        int sum,sub,div,quot;
+        sum = n1+n2;
+        sub = n1-n2;
+        div = n1/n2;
+        quot = n1%n2;
+        System.out.println("Sum :"+sum);
+        System.out.println("Diffrence :"+sub);
+        System.out.println("Division :"+div);
+        System.out.println("Quotient :"+quot);
+    }
+}
